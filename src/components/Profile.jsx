c5193492fb7e1e0971a47f2bc5351bb24fc111d5
KudrinOleg <olegkudrin1995@gmail.com>
2022-05-12 19:16:36 +0700

    Added_new_file

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+const Profile = () =>{
+    return         <div className='content'>
+    <div>
+        <img src=''></img>
+    </div>
+    <div>
+        ava+description
+    </div>
+    <div>
+      my posts
+      <div>
+        new post
+    </div>
+    <div>
+      Post 1
+    </div><div>
+      Post 2
+    </div>
+    </div>
+  </div>
+}
+
+export default Profile;
